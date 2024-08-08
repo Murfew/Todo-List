930d6cbd111944ca0de17be9894e4635fe75c581
Murfew <matthew.hurley953@gmail.com>
2024-08-08 15:06:05 -0400

    initial commit
    created the webpack config files as well as the basic html, js, and css files

---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+// npm init -y
+// npm install --save-dev webpack webpack-cli style-loader css-loader html-webpack-plugin webpack-dev-server webpack-merge
+//    import all files into js
+// npm run build to create a production build
+// npm start to host live server of development build
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: "body",
+    }),
+  ],
+};
