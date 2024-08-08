4c30af9fd259f149a3768ac78ff1a8b47f3ee648
Murfew <matthew.hurley953@gmail.com>
2024-08-08 16:37:32 -0400

    Fixed the remove and add project functionalities to have a better structure in the storage

--- src/appLogic.js
+++ src/appLogic.js
@@ -2,14 +2,13 @@ import Project from "./Project";
 import Todo from "./Todo";
 
 export function initializeStorage() {
-  localStorage.setItem("projects", JSON.stringify([]));
-  localStorage.setItem("todos", JSON.stringify([]));
-
   createProject("Default");
 }
 
-function createProject(title) {
-  const projects = JSON.parse(localStorage.getItem("projects"));
-  projects.push(new Project(title));
-  localStorage.setItem("projects", JSON.stringify(projects));
+export function createProject(title) {
+  localStorage.setItem(title, JSON.stringify(new Project(title)));
+}
+
+export function removeProject(title) {
+  localStorage.removeItem(title);
 }
