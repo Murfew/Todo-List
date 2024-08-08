2e7772ce6c7172ab0fdd9df88c4bb61fb1e09986
Murfew <matthew.hurley953@gmail.com>
2024-08-08 15:48:45 -0400

    created Todo and Project classes
    These will be the data structures the app will use. Projects containing Todos.

---
+++ src/Project.js
@@ -0,0 +1,14 @@
+class Project {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  addTodo(todo) {
+    this.todos.push(todo);
+  }
+
+  removeTodo(todo) {
+    this.todos.splice(this.todos.indexOf(todo), 1);
+  }
+}
