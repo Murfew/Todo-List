8924efa96007b09166556dd905281a1e34c26fb3
Murfew <matthew.hurley953@gmail.com>
2024-08-09 08:46:32 -0400

    added my basic html structure

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,9 @@
 export default class Todo {
-  constructor(title, description, dueDate, priority, notes) {
+  constructor(title, description = "", dueDate = undefined, priority = 0) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
-    this.notes = notes;
     this.complete = false;
   }
 }
